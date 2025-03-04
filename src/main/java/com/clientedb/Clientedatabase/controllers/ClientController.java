25a2b1a6119a8602c0c4f364c6dd95ece0c44a0c
Hugo <hugo.mmoreira@outlook.com>
2025-03-04 18:21:21 -0300

    implementação do metodo findAll

--- src/main/java/com/clientedb/Clientedatabase/controllers/ClientController.java
+++ src/main/java/com/clientedb/Clientedatabase/controllers/ClientController.java
@@ -1,6 +1,9 @@
 package com.clientedb.Clientedatabase.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +20,14 @@ public class ClientController {
 	private ClientService service;
 	
 	@GetMapping(value = "/{id}")
-	public ClientDTO findById(@PathVariable Long id) {
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO result = service.findById(id);
-		return result;
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+		Page<ClientDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);
 	}
 }
--- src/main/java/com/clientedb/Clientedatabase/services/ClientService.java
+++ src/main/java/com/clientedb/Clientedatabase/services/ClientService.java
@@ -1,6 +1,8 @@
 package com.clientedb.Clientedatabase.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,11 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 		Client result = repo.findById(id).get();
 		return new ClientDTO(result);
-		
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable){
+		Page<Client> dto = repo.findAll(pageable);
+		return dto.map(x -> new ClientDTO(x));
 	}
 }
